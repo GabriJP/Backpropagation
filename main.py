9c0cc740ff827df0e6a83087f497167e3967a951
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2016-11-15 14:32:07 +0000

    Con iteraciones para la media

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 import numpy as np
 import string
+import statistics
 
 TARGET_ERROR = 0.2
 INPUT_FILE = "Input/letters.txt"
 DELIMITER = " "
 INPUT_LAYER_WIDTH = 7
 INPUT_LAYER_HEIGHT = 5
-HIDDEN_LAYER_WIDTH = 1
-HIDDEN_LAYER_HEIGHT = 10
 OUTPUT_LAYER_WIDTH = 1
 OUTPUT_LAYER_HEIGHT = 26
 ALPHABET = list(string.ascii_uppercase)
@@ -23,42 +22,54 @@ def one_hot(l, n):
     return o_h
 
 
-data = np.genfromtxt(INPUT_FILE, delimiter=DELIMITER, dtype=int)
-np.random.shuffle(data)
-x_data = data[:, 0:35]
-y_data = one_hot(data[:, 35], 26)
+def net(learning_rate, number_of_hidden_elements):
+	print("net(%f, %d)" % (learning_rate, number_of_hidden_elements))
 
-x = tf.placeholder(tf.float32, [None, 35])
-y_ = tf.placeholder(tf.float32, [None, 26])
+	data = np.genfromtxt(INPUT_FILE, delimiter=DELIMITER, dtype=int)
+	np.random.shuffle(data)
+	x_data = data[:, 0:35]
+	y_data = one_hot(data[:, 35], 26)
 
-W1 = tf.Variable(
-    np.float32(np.random.rand(INPUT_LAYER_WIDTH * INPUT_LAYER_HEIGHT, HIDDEN_LAYER_WIDTH * HIDDEN_LAYER_HEIGHT)) * 0.1)
-W2 = tf.Variable(np.float32(
-    np.random.rand(HIDDEN_LAYER_WIDTH * HIDDEN_LAYER_HEIGHT, OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
-b1 = tf.Variable(np.float32(np.random.rand(HIDDEN_LAYER_WIDTH * HIDDEN_LAYER_HEIGHT)) * 0.1)
-b2 = tf.Variable(np.float32(np.random.rand(OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
-y1 = tf.sigmoid(tf.matmul(x, W1) + b1)
-y2 = tf.nn.softmax(tf.matmul(y1, W2) + b2)
+	x = tf.placeholder(tf.float32, [None, 35])
+	y_ = tf.placeholder(tf.float32, [None, 26])
 
-cross_entropy = tf.reduce_sum(tf.square(y_ - y2))
-train = tf.train.GradientDescentOptimizer(0.9).minimize(cross_entropy)
-init = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init)
+	W1 = tf.Variable(
+    		np.float32(np.random.rand(INPUT_LAYER_WIDTH * INPUT_LAYER_HEIGHT, number_of_hidden_elements)) * 0.1)
+	W2 = tf.Variable(np.float32(
+    		np.random.rand(number_of_hidden_elements, OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
+	b1 = tf.Variable(np.float32(np.random.rand(number_of_hidden_elements)) * 0.1)
+	b2 = tf.Variable(np.float32(np.random.rand(OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
+	y1 = tf.sigmoid(tf.matmul(x, W1) + b1)
+	y2 = tf.nn.softmax(tf.matmul(y1, W2) + b2)
 
-print("----------------------")
-print("   Start training...  ")
-print("----------------------")
+	cross_entropy = tf.reduce_sum(tf.square(y_ - y2))
+	train = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+	init = tf.initialize_all_variables()
+	sess = tf.Session()
+	sess.run(init)
 
-errors = [sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data})]
+#	print("----------------------")
+#	print("   Start training...  ")
+#	print("----------------------")
 
-while errors[-1] > TARGET_ERROR:
-    sess.run(train, feed_dict={x: x_data, y_: y_data})
-    errors.append(sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data}))
+	errors = [sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data})]
 
-    if len(errors) % 50 is 0:
-        print("Iteration #:", len(errors), "Error: ", errors[-1])
+	while errors[-1] > TARGET_ERROR:
+    		sess.run(train, feed_dict={x: x_data, y_: y_data})
+    		errors.append(sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data}))
 
-if len(errors) % 50 is not 0:
-    print("Iteration #:", len(errors), "Error: ", errors[-1])
-    print(errors[-20:])
+#    		if len(errors) % 50 is 0:
+#        		print("Iteration #:", len(errors), "Error: ", errors[-1])
+
+#	if len(errors) % 50 is not 0:
+#		print("Iteration #:", len(errors), "Error: ", errors[-1])
+#		print(errors[-20:])
+	return len(errors)
+
+
+for number_of_elements in range(7, 21):
+	current = []
+	for i in range(10):
+		current.append(net(0.9, number_of_elements))
+	print("For %d elements in the hidden layer, it was needed about %f iterations" % (number_of_elements, statistics.median(current)))
+
