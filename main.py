ab224cfb69a900748b850b500ba8d6830c760d28
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2016-11-16 00:08:13 +0000

    Se automatiza la recogida de datos

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import statistics
 TARGET_ERROR = 0.2
 INPUT_FILE = "Input/letters.txt"
 DELIMITER = " "
-INPUT_LAYER_WIDTH = 7
-INPUT_LAYER_HEIGHT = 5
+INPUT_LAYER_WIDTH = 5
+INPUT_LAYER_HEIGHT = 7
 OUTPUT_LAYER_WIDTH = 1
 OUTPUT_LAYER_HEIGHT = 26
 ALPHABET = list(string.ascii_uppercase)
+MAX_ITERATIONS = 10000
 
 
 def one_hot(l, n):
+    """Returns an array of arrays of zeros, each internal array with one 1 (the element of the list)"""
     if type(l) == list:
         l = np.array(l)
     l = l.flatten()
@@ -22,54 +24,82 @@ def one_hot(l, n):
     return o_h
 
 
+def get_pattern(pattern, width, height):
+    """Returns the pattern in a string of width*height figure"""
+    if len(pattern) != width * height:
+        raise Exception("len(pattern) != width*height")
+    result = ""
+    for row in range(height):
+        for column in range(width):
+            result += ' ' if pattern[row * width + column] == 0 else '·'
+        result += '\n'
+    return result
+
+
 def net(learning_rate, number_of_hidden_elements):
-	print("net(%f, %d)" % (learning_rate, number_of_hidden_elements))
+    print("net(%f, %d)" % (learning_rate, number_of_hidden_elements))
 
-	data = np.genfromtxt(INPUT_FILE, delimiter=DELIMITER, dtype=int)
-	np.random.shuffle(data)
-	x_data = data[:, 0:35]
-	y_data = one_hot(data[:, 35], 26)
+    data = np.genfromtxt(INPUT_FILE, delimiter=DELIMITER, dtype=int)
+    np.random.shuffle(data)
+    x_data = data[:, 0:35]
+    # y_data = x_data
+    y_data = one_hot(data[:, 35], 26)
 
-	x = tf.placeholder(tf.float32, [None, 35])
-	y_ = tf.placeholder(tf.float32, [None, 26])
+    # for index in range(20):
+    #     input_data = get_pattern(x_data[index], INPUT_LAYER_WIDTH, INPUT_LAYER_HEIGHT)
+    #     output_data = get_pattern(y_data[index], INPUT_LAYER_WIDTH, INPUT_LAYER_HEIGHT)
+    #     print(input_data[:-1] + '  -> ' + output_data[:-1] + '\n')
 
-	W1 = tf.Variable(
-    		np.float32(np.random.rand(INPUT_LAYER_WIDTH * INPUT_LAYER_HEIGHT, number_of_hidden_elements)) * 0.1)
-	W2 = tf.Variable(np.float32(
-    		np.random.rand(number_of_hidden_elements, OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
-	b1 = tf.Variable(np.float32(np.random.rand(number_of_hidden_elements)) * 0.1)
-	b2 = tf.Variable(np.float32(np.random.rand(OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
-	y1 = tf.sigmoid(tf.matmul(x, W1) + b1)
-	y2 = tf.nn.softmax(tf.matmul(y1, W2) + b2)
+    x = tf.placeholder(tf.float32, [None, INPUT_LAYER_WIDTH * INPUT_LAYER_HEIGHT])
+    y_ = tf.placeholder(tf.float32, [None, OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT])
 
-	cross_entropy = tf.reduce_sum(tf.square(y_ - y2))
-	train = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
-	init = tf.initialize_all_variables()
-	sess = tf.Session()
-	sess.run(init)
+    "Initialization of weights for hidden and output layers"
+    W1 = tf.Variable(
+        np.float32(np.random.rand(INPUT_LAYER_WIDTH * INPUT_LAYER_HEIGHT, number_of_hidden_elements)) * 0.1)
+    W2 = tf.Variable(
+        np.float32(np.random.rand(number_of_hidden_elements, OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
 
-#	print("----------------------")
-#	print("   Start training...  ")
-#	print("----------------------")
+    "Initialization ob bias for hidden and output layers"
+    b1 = tf.Variable(np.float32(np.random.rand(number_of_hidden_elements)) * 0.1)
+    b2 = tf.Variable(np.float32(np.random.rand(OUTPUT_LAYER_WIDTH * OUTPUT_LAYER_HEIGHT)) * 0.1)
 
-	errors = [sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data})]
+    "Output of hidden and output layers (activation function)"
+    y1 = tf.sigmoid(tf.matmul(x, W1) + b1)
+    y2 = tf.nn.softmax(tf.matmul(y1, W2) + b2)
 
-	while errors[-1] > TARGET_ERROR:
-    		sess.run(train, feed_dict={x: x_data, y_: y_data})
-    		errors.append(sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data}))
+    "Function to reduce"
+    cross_entropy = tf.reduce_sum(tf.square(y_ - y2))
+    train = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
-#    		if len(errors) % 50 is 0:
-#        		print("Iteration #:", len(errors), "Error: ", errors[-1])
+    "TF initialization"
+    sess = tf.Session()
+    sess.run(tf.initialize_all_variables())
 
-#	if len(errors) % 50 is not 0:
-#		print("Iteration #:", len(errors), "Error: ", errors[-1])
-#		print(errors[-20:])
-	return len(errors)
+    errors = [sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data})]
+
+    "Training"
+    while errors[-1] > TARGET_ERROR and len(errors) < MAX_ITERATIONS:
+        sess.run(train, feed_dict={x: x_data, y_: y_data})
+        errors.append(sess.run(cross_entropy, feed_dict={x: x_data, y_: y_data}))
+
+        # if len(errors) % 50 is 0:
+        #     print("Iteration #:", len(errors), "Error: ", errors[-1])
+
+    # if len(errors) % 50 is not 0:
+    #     print("Iteration #:", len(errors), "Error: ", errors[-1])
+    #     print(errors[-20:])
+    if len(errors) == MAX_ITERATIONS:
+        print("Max iterations reached, retrying")
+        return net(learning_rate, number_of_hidden_elements)
+    else:
+        return len(errors)
 
 
-for number_of_elements in range(7, 21):
-	current = []
-	for i in range(10):
-		current.append(net(0.9, number_of_elements))
-	print("For %d elements in the hidden layer, it was needed about %f iterations" % (number_of_elements, statistics.median(current)))
-
+for rate in np.arange(0.05, 1.5, 0.05):
+    for number_of_elements in range(5, 15):
+        current = []
+        for i in range(10):
+            current.append(net(rate, number_of_elements))
+        print(
+            "For %d elements in the hidden layer and a learning rate of %f, it was needed a median of %f iterations" % (
+                number_of_elements, rate, statistics.median(current)))
