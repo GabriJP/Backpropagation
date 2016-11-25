88d9334124de942bef404282346a6adac0de09c1
Gabriel <gabriel.jimenez102@alu.ulpgc.es>
2016-11-25 16:24:35 +0000

    Aumento iteraciones y mejoro el multihilo

--- main.py
+++ main.py
@@ -27,28 +27,25 @@ for capas in [net_1capa, net_2capas]:
                     "una capa oculta" if capas == net_1capa else "dos capas ocultas", noise,
                     "gradiente" if func == 1 else "gradiente con momento %01.1f" % momentum)])
 
+                neuronas_por_capa = [(capa1, 0) for capa1 in n_neuronas] if net_1capa == capas else \
+                    [(capa1, capa2) for capa1 in n_neuronas for capa2 in n_neuronas]
+
+                writer.writerow(['ratio'] +
+                                [str(capa1) for capa1 in n_neuronas] if capas == net_1capa else
+                                ["%d - %d" % (capa1, capa2) for capa1 in n_neuronas for capa2 in n_neuronas])
+                for ratio in np.arange(0.2, 1.1, 0.1):
+                    with ThreadPool(processes=1024) as pool:
+                        results = [
+                            pool.apply_async(ejecuta, (capas, ratio, neu_1, noise, func, momentum, neu_2)) for
+                            neu_1, neu_2 in neuronas_por_capa]
+                        pool.close()
+                        pool.join()
+                        writer.writerow([ratio] + [r.get() for r in results])
+                        csvfile.flush(
+
+                        )
                 writer.writerow([])
-                if capas == net_1capa:
-                    imp = [str(capa1) for capa1 in n_neuronas]
-                else:
-                    imp = ["%d - %d" % (capa1, capa2) for capa1 in n_neuronas for capa2 in n_neuronas]
-                writer.writerow(['ratio'] + imp)
                 csvfile.flush()
-                for ratio in np.arange(0.2, 1.4, 0.1):
-                    if capas == net_1capa:
-                        result = [(capa1, 0) for capa1 in n_neuronas]
-                    else:
-                        result = [(capa1, capa2) for capa1 in n_neuronas for capa2 in n_neuronas]
-
-                    pool = ThreadPool(processes=325)
-                    results = [pool.apply_async(ejecuta, (capas, ratio, neu_1, noise, func, momentum, neu_2)) for
-                               neu_1, neu_2 in result]
-                    pool.close()
-                    pool.join()
-                    writer.writerow([ratio] + [result.get() for result in results])
-                    csvfile.flush()
-                writer.writerow([])
-
 csvfile.flush()
 csvfile.close()
 
